caef2fbd665ac7aa0cf9f7f1b6aa50433d5a7b39
Glitch (discordjsv12basitaltyapi) <none>
2021-10-04 17:38:23 +0000

    ✌️👀 Checkpoint ./ayarlar.json:23951151/72 ./bot.js:23951151/194

--- bot.js
+++ bot.js
@@ -161,7 +161,11 @@ client.on("messageUpdate", msg => {
                        "piç",
                        "orospu çocuğu",
                        "orospu",
-                       "oruspu"];
+                       "ananı",
+                       "aq",
+                      "mq",                   
+                      "anan",
+                      "oruspu"];
         if (kufur.some(word => msg.content.includes(word))) {
           try {
             if (!msg.member.hasPermission("BAN_MEMBERS")) {
@@ -208,7 +212,7 @@ client.on('message', async msg => {
       const streamOptions = { seek: 0, volume: 50 };
       var voiceChannel = msg.member.voiceChannel;
         voiceChannel.join().then(connection => {
-        const stream = ('https://youtu.be/gRceno9Tq3k', { filter : 'audioonly' });
+        const stream = ('Thanos ❤️Twilight', { filter : 'audioonly' });
         const dispatcher = connection.playStream(stream, streamOptions);
         })
   }
